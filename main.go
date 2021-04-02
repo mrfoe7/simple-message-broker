9a19eda81094816cf9e05f8a30f1129e957de37f
Saimon Shaplygin <semyuon@gmail.com>
2021-04-02 11:20:16 +0300

    fix: ctx timeout

--- main.go
+++ main.go
@@ -223,7 +223,7 @@ func main() {
 
 	log.Println("server stopped")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
 	defer func() {
 		cancel()
